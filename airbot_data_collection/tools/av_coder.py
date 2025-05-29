009393c2ce99ab0e201e6d8d5c8ed778e94a805b
Haizhou Ge <1352674740@qq.com>
2025-05-30 07:55:32 +0800

    fix encode h264

--- airbot_data_collection/tools/av_coder.py
+++ airbot_data_collection/tools/av_coder.py
@@ -27,21 +27,24 @@ def read_all_frames_pyav(video_path: str) -> List[np.ndarray]:
 
 
 def encode_h264(data: List[Dict[str, Union[int, bytes]]]) -> bytes:
-    if isinstance(data[0]["data"], bytes):
+    first_frame = data[0]["data"]
+    if isinstance(first_frame, bytes):
         jpeg = TurboJPEG()
         preprocess = jpeg.decode
+        first_frame = preprocess(first_frame)
     else:
         # No preprocessing needed if data is already in numpy format
         preprocess = lambda x: x
+    height, width = first_frame.shape[:2]
     outbuf = BytesIO()
     # "ultrafast", "superfast", "veryfast", "faster", "fast"
-    container = av.open(outbuf, "w", format="mp4", options={"preset": "fast"})
+    container = av.open(outbuf, "w", format="mp4")
     stream = container.add_stream("h264", options={"preset": "fast"})
-    stream.width = data[0]["data"].shape[1]
-    stream.height = data[0]["data"].shape[0]
+    stream.width = width
+    stream.height = height
     stream.pix_fmt = "yuv420p"
     # The time base is set to 1e-9 second (i.e. timestamps are in nanoseconds)
-    stream.time_base = fractions.Fraction(1, 1e9)
+    stream.time_base = fractions.Fraction(1, int(1e9))
     start_time = data[0]["t"]
     last_time = start_time
     time_base = fractions.Fraction(1, 1000)
@@ -55,9 +58,9 @@ def encode_h264(data: List[Dict[str, Union[int, bytes]]]) -> bytes:
             logger.warning(
                 f"Frame timestamp {cur_time} is not greater than last timestamp {last_time}. Adjusting."
             )
-            cur_time = last_time + 1e9 / 60
-        video_frame.pts = cur_time - start_time
+            # cur_time = last_time + int(1e9 / 60)
         last_time = cur_time
+        video_frame.pts = cur_time - start_time
         video_frame.time_base = time_base
         for packet in stream.encode(video_frame):
             container.mux(packet)
