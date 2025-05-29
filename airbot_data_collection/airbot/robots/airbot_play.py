7231b65738144d13ff34de132b4b35fb2d945c21
Haizhou Ge <1352674740@qq.com>
2025-05-29 15:10:21 +0800

    fix RepeatedScalarContainer in arm get info

--- airbot_data_collection/airbot/robots/airbot_play.py
+++ airbot_data_collection/airbot/robots/airbot_play.py
@@ -4,7 +4,7 @@ from airbot_py.arm import AIRBOTArm, RobotMode, SpeedProfile
 from pydantic import BaseModel, PositiveInt
 
 from airbot_data_collection.basis import System, SystemMode
-
+from google.protobuf.internal.containers import RepeatedScalarFieldContainer
 from time import time_ns
 
 
@@ -78,7 +78,14 @@ class AIRBOTPlay(System):
         return self.interface.disconnect()
 
     def get_info(self):
-        return self.interface.get_product_info() | {
+        return {
+            key: (
+                list(value)
+                if isinstance(value, RepeatedScalarFieldContainer)
+                else value
+            )
+            for key, value in self.interface.get_product_info().items()
+        } | {
             "arm/joint_names": [f"joint{i}" for i in range(1, 7)],
             "arm_eef/joint_names": ["arm_eef_gripper_joint"],
         }
