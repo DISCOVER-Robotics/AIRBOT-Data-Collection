587a59096d6c306d160c67a41531414bd6142491
Haizhou Ge <1352674740@qq.com>
2025-06-20 18:42:25 +0800

    add realsense install

--- install.sh
+++ install.sh
@@ -4,5 +4,19 @@
 
 set -e
 
+arg=$1
+
 sudo apt-get install -y libturbojpeg gcc python3-dev v4l-utils
 pip install -e ."[all]" -i https://pypi.mirrors.ustc.edu.cn/simple
+
+if  [ "$arg" == "realsense" ]; then
+    PYTHON_VERSION=$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
+    PYTHON_MAJOR=$(echo "$PYTHON_VERSION" | cut -d. -f1)
+    PYTHON_MINOR=$(echo "$PYTHON_VERSION" | cut -d. -f2)
+
+    if [ "$PYTHON_MAJOR" -eq 3 ] && [ "$PYTHON_MINOR" -lt 12 ]; then
+        pip install pyrealsense2
+    else
+        pip install pyrealsense2-beta
+    fi
+fi
