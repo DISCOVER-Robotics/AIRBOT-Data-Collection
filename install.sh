e647cc4a7e2abb70f0a7618ee4fd5538c1ab7e9a
Haizhou Ge <1352674740@qq.com>
2025-07-31 14:15:21 +0800

    remove deps of v4l-utils and upgrade pip, setuptools and wheel first

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ set -e
 
 arg=$1
 
-sudo apt-get install -y pip python3-dev libturbojpeg gcc v4l-utils
+sudo apt-get install -y pip python3-dev libturbojpeg gcc
+pip install --upgrade pip setuptools wheel -i https://pypi.mirrors.ustc.edu.cn/simple
 pip install -e ."[all]" -i https://pypi.mirrors.ustc.edu.cn/simple
 
 if  [ "$arg" == "realsense" ]; then
