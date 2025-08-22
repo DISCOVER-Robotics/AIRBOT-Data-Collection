611014559e1b58116d4ac902fd2fbbeb4c7cb479
Haizhou Ge <1352674740@qq.com>
2025-08-22 15:57:32 +0800

    improve test rs depth log

--- airbot_data_collection/tests/test_realsense_depth.py
+++ airbot_data_collection/tests/test_realsense_depth.py
@@ -23,6 +23,7 @@ def main():
     pipeline = rs.pipeline()
     config = rs.config()
 
+    print("Enabling device...")
     if args.device_id:
         config.enable_device(args.device_id)
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
@@ -37,9 +38,10 @@ def main():
 
     pipeline.start(config)
 
+    print("Device enabled. Starting stream...")
     try:
         while True:
-            frames = pipeline.wait_for_frames()
+            frames = pipeline.wait_for_frames(3000)
             if align_depth:
                 frames = align.process(frames)
             color_frame = frames.get_color_frame()
