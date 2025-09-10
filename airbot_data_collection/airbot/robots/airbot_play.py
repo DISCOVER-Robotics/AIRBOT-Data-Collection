c973631d9b54e9f6c9b96079a572a205060e5e76
Haizhou Ge <1352674740@qq.com>
2025-09-10 15:34:30 +0800

    remove pose shake fixme

--- airbot_data_collection/airbot/robots/airbot_play.py
+++ airbot_data_collection/airbot/robots/airbot_play.py
@@ -240,7 +240,6 @@ class AIRBOTPlay(System):
     ) -> dict[str, dict[str, Union[float, Dict[str, List[float]]]]]:
         """key: component_name/data_type"""
         obs = {}
-        # FIXME: Currently, the robot arm will have a large shake when acquiring pose
         if self.config.pose_observation:
             start = perf_counter()
             pose = self.interface.get_end_pose()
