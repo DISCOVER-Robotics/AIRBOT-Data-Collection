7aefedafe30f870439e9379d02c7886c09cbb38a
Haizhou Ge <1352674740@qq.com>
2025-11-04 19:39:56 +0800

    muti caller now support different output array types

--- airbot_data_collection/common/callers/multi.py
+++ airbot_data_collection/common/callers/multi.py
@@ -1,9 +1,39 @@
 from pydantic import BaseModel, PositiveInt, Field
 from collections.abc import Callable
-from typing import List, Optional
+from typing import List, Optional, Literal
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
 from airbot_data_collection.common.callers.basis import CallerBasis
 from airbot_data_collection.basis import ConcurrentMode
+from airbot_data_collection.common.utils.array_like import (
+    ArrayInfo,
+    array_namespace,
+    get_namespace_by_name,
+    get_array_type_by_ns_name,
+    get_tensor_device_auto,
+)
+
+
+class ScalarsToContainerConfig(BaseModel):
+    """Configuration for converting scalar outputs to a container."""
+
+    xp: Literal["list", "numpy", "torch"] = "list"
+    """The container type to convert scalars into."""
+    dtype: str = ""
+    """The data type of the container elements."""
+    device: str = ""
+    """The device of the container."""
+
+    def model_post_init(self, context):
+        if self.xp != "list":
+            if not self.dtype:
+                if self.xp == "numpy":
+                    self.dtype = "float64"
+                elif self.xp == "torch":
+                    self.dtype = "float32"
+            if self.xp == "numpy":
+                self.device = "cpu"
+            elif self.xp == "torch" and not self.device:
+                self.device = get_tensor_device_auto()
 
 
 class MultiCallerConfig(BaseModel):
@@ -15,6 +45,11 @@ class MultiCallerConfig(BaseModel):
     """Number of worker threads to use. 1 means no parallelism (no executor).
     None means using as many workers as callables."""
     mode: ConcurrentMode = ConcurrentMode.thread
+    """Mode of parallelism: thread or process."""
+    scalar_container: ScalarsToContainerConfig = ScalarsToContainerConfig()
+    """The container type for scalar outputs (int or float). Otherwise, the output
+    type will be used if it is array-like. If the output is not array-like, a list
+    container will be used."""
 
 
 class MultiCaller(CallerBasis):
@@ -31,20 +66,52 @@ class MultiCaller(CallerBasis):
                 else ProcessPoolExecutor(max_workers)
             )
             self._call = self._call_in_parallel
-            self._p_outputs = [None] * len(self.config.callables)
         else:
             self._call = self._call_in_sequence
+        self._first_call = True
         return True
 
     def reset(self):
         """Reset the internal state of the caller, if any."""
 
-    def _call_in_sequence(self, *args, **kwds):
-        outputs = []
+    def _first_setup(self, *args, **kwds):
+        info_set = set()
+        has_scalar = False
         for func in self.config.callables:
             output = func(*args, **kwds)
-            outputs.append(output)
-        return outputs
+            if isinstance(output, (int, float)):
+                has_scalar = True
+            else:
+                info_set.add(ArrayInfo.from_array(output))
+        if len(info_set) > 1:
+            print(f"The outputs of the callables have different array info: {info_set}")
+        num_calls = len(self.config.callables)
+        scalar_cfg = self.config.scalar_container
+        if len(info_set) > 1 or (has_scalar and scalar_cfg.xp == "list"):
+            self._p_outputs = [None] * num_calls
+        else:
+            if has_scalar:
+                info = ArrayInfo(
+                    arr_type=get_array_type_by_ns_name(scalar_cfg.xp),
+                    shape=(),
+                    dtype=scalar_cfg.dtype,
+                    device=scalar_cfg.device,
+                )
+                xp = get_namespace_by_name(scalar_cfg.xp)
+            else:
+                info: ArrayInfo = info_set.pop()
+                xp = array_namespace(output)
+            self._p_outputs = xp.zeros(
+                (num_calls,) + info.shape,
+                dtype=getattr(xp, info.dtype),
+                device=info.device,
+            )
+
+    def _call_in_sequence(self, *args, **kwds):
+        for i, func in enumerate(self.config.callables):
+            output = func(*args, **kwds)
+            self._p_outputs[i] = output
+        return self._p_outputs
 
     def _call_in_parallel(self, *args, **kwds):
         future_to_index = {
@@ -58,6 +125,9 @@ class MultiCaller(CallerBasis):
 
     def __call__(self, *args, **kwds):
         """Call all the callables in sequence and aggregate their outputs."""
+        if self._first_call:
+            self._first_setup(*args, **kwds)
+            self._first_call = False
         return self._call(*args, **kwds)
 
 
@@ -67,14 +137,29 @@ if __name__ == "__main__":
         lambda x: x * 2,
         lambda x: x**2,
     ]
+    # import numpy as np
+    # import torch
+
+    # xp = torch  # np or torch
+    # device = "cpu" if xp is np else torch.device("cuda")
+
+    # callables = [
+    #     lambda x: xp.ones((2, 2), device=device) * (x + 1),
+    #     lambda x: xp.ones((2, 2), device=device) * x * 2,
+    #     lambda x: xp.ones((2, 2), device=device) * x**2,
+    # ]
 
     config = MultiCallerConfig(
-        callables=callables, num_workers=None, mode=ConcurrentMode.thread
+        callables=callables,
+        num_workers=None,
+        mode=ConcurrentMode.thread,
+        scalar_container=ScalarsToContainerConfig(xp="torch"),
     )
     multi_caller = MultiCaller(config=config)
     multi_caller.on_configure()
     result = multi_caller(3)
     print(result)  # Expected output: [4, 6, 9]
+    print(ArrayInfo.from_array(result))
     # Test with no parallelism
     config_no_parallel = MultiCallerConfig(callables=callables, num_workers=1)
     multi_caller_no_parallel = MultiCaller(config=config_no_parallel)
---
+++ airbot_data_collection/common/utils/array_like.py
@@ -0,0 +1,90 @@
+from array_api_compat import array_namespace  # noqa: F401
+from pydantic import BaseModel, computed_field
+from typing import Any, Type, Tuple
+from typing_extensions import Self, TYPE_CHECKING
+import importlib
+
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+    from torch import Tensor
+    from typing import Union
+
+    Array = Union[NDArray, Tensor]
+else:
+    from typing import MutableSequence
+
+    Array = MutableSequence
+
+
+class ArrayInfo(BaseModel, frozen=True):
+    """Information about an array-like object."""
+
+    arr_type: Type
+    """The type of the array-like object."""
+    dtype: Any
+    """The data type of the array-like object."""
+    shape: Tuple[int, ...]
+    """The shape of the array-like object."""
+    device: Any
+    """The device of the array-like object."""
+
+    @computed_field
+    @property
+    def type_name(self) -> str:
+        """The name of the type of the array-like object."""
+        return self.arr_type.__name__
+
+    @classmethod
+    def from_array(cls, array: Array) -> Self:
+        """Create an ArrayInfo from an array-like object."""
+        return cls(
+            type=type(array), dtype=array.dtype, shape=array.shape, device=array.device
+        )
+
+
+def get_namespace_by_name(name: str):
+    """Get the array namespace by name."""
+    try:
+        if TYPE_CHECKING:
+            import numpy as np
+
+            return np
+        else:
+            return importlib.import_module(f"array_api_compat.{name}")
+    except ImportError as e:
+        raise ValueError(f"Backend '{name}' is not available or not installed.") from e
+
+
+def get_array_type_by_ns_name(name: str) -> Type:
+    """Get the array type by name."""
+    if name == "numpy":
+        from numpy import ndarray
+
+        return ndarray
+    elif name == "torch":
+        from torch import Tensor
+
+        return Tensor
+    else:
+        return str
+
+
+def get_tensor_device_auto() -> str:
+    import torch
+
+    return f"cuda:{torch.cuda.current_device()}" if torch.cuda.is_available() else "cpu"
+
+
+if __name__ == "__main__":
+    import numpy as np
+    import torch
+
+    arr_np = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float32)
+    arr_torch = torch.tensor([[1, 2, 3], [4, 5, 6]], dtype=torch.float32)
+
+    info_np = ArrayInfo.from_array(arr_np)
+    info_torch = ArrayInfo.from_array(arr_torch)
+
+    print("NumPy Array Info:", info_np)
+    print("Torch Tensor Info:", info_torch)
