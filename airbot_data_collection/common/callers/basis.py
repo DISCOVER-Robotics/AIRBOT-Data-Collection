fe1de25fade92e94d30e6f4e9b8de5aa06b2e65c
Haizhou Ge <1352674740@qq.com>
2025-11-07 13:42:16 +0800

    support multi inputs

--- airbot_data_collection/common/callers/basis.py
+++ airbot_data_collection/common/callers/basis.py
@@ -51,7 +51,10 @@ class MockCaller(CallerBasis):
 
 
 class CallerChainConfig(BaseModel):
-    callers: List[Callable]
+    callables: List[Callable]
+    """List of callables to be chained together."""
+    single_input: bool = True
+    """Whether the input to the chain is a single value or are args & kwargs."""
 
 
 class CallerChain(CallerBasis):
@@ -63,20 +66,38 @@ class CallerChain(CallerBasis):
         return True
 
     def reset(self):
-        for caller in self.config.callers:
+        for caller in self.config.callables:
             if hasattr(caller, "reset"):
                 caller.reset()
 
-    def __call__(self, input: Any) -> Any:
+    def _single_call(self, input: Any) -> Any:
         output = input
-        for caller in self.config.callers:
+        for caller in self.config.callables:
             output = caller(output)
         return output
+
+    def _multi_call(self, *args, **kwds) -> Any:
+        first = True
+        for caller in self.config.callables:
+            if first:
+                output = caller(*args, **kwds)
+                first = False
+            else:
+                output = caller(output)
+        return output
+
+    def __call__(self, *args, **kwds) -> Any:
+        if self.config.single_input:
+            return self._single_call(*args, **kwds)
+        else:
+            return self._multi_call(*args, **kwds)
 
 
 if __name__ == "__main__":
     caller_chain = CallerChain(
-        config=CallerChainConfig(callers=[lambda x: x + 1, lambda x: x * 2])
+        config=CallerChainConfig(
+            callables=[lambda x: x + 1, lambda x: x * 2], single_input=False
+        )
     )
     caller_chain.configure()
-    print(caller_chain(0.0))  # Should print 2.0
+    print(caller_chain(x=0.0))  # Should print 2.0
