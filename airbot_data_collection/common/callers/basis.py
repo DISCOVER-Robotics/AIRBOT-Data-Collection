15bc7b56347060c32fe63ef6e4983133664893e1
Haizhou Ge <1352674740@qq.com>
2025-11-07 13:59:29 +0800

    add output_chain in CallerChain

--- airbot_data_collection/common/callers/basis.py
+++ airbot_data_collection/common/callers/basis.py
@@ -1,8 +1,11 @@
 from abc import abstractmethod
-from typing import Any, Tuple, Literal, List
+from typing import Any, Tuple, Literal, List, Generic, TypeVar
 from pydantic import BaseModel
 from airbot_data_collection.basis import ConfigurableBasis
 from collections.abc import Callable
+
+
+T = TypeVar("T")
 
 
 class CallerBasis(ConfigurableBasis):
@@ -57,26 +60,34 @@ class CallerChainConfig(BaseModel):
     """Whether the input to the chain is a single value or are args & kwargs."""
 
 
-class CallerChain(CallerBasis):
+class CallerChain(CallerBasis, Generic[T]):
     """A caller that chains multiple callers together."""
 
     config: CallerChainConfig
 
     def on_configure(self):
+        self.output_chain = []
+        for caller in self.config.callables:
+            if isinstance(caller, CallerBasis):
+                if not caller.configure():
+                    self.get_logger().error(f"Failed to configure caller: {caller}")
+                    return False
         return True
 
     def reset(self):
         for caller in self.config.callables:
-            if hasattr(caller, "reset"):
+            if isinstance(caller, CallerBasis):
                 caller.reset()
+        self.output_chain = []
 
-    def _single_call(self, input: Any) -> Any:
+    def _single_call(self, input: Any) -> T:
         output = input
         for caller in self.config.callables:
             output = caller(output)
+            self.output_chain.append(output)
         return output
 
-    def _multi_call(self, *args, **kwds) -> Any:
+    def _multi_call(self, *args, **kwds) -> T:
         first = True
         for caller in self.config.callables:
             if first:
@@ -84,9 +95,10 @@ class CallerChain(CallerBasis):
                 first = False
             else:
                 output = caller(output)
+            self.output_chain.append(output)
         return output
 
-    def __call__(self, *args, **kwds) -> Any:
+    def __call__(self, *args, **kwds) -> T:
         if self.config.single_input:
             return self._single_call(*args, **kwds)
         else:
@@ -101,3 +113,5 @@ if __name__ == "__main__":
     )
     caller_chain.configure()
     print(caller_chain(x=0.0))  # Should print 2.0
+    print(caller_chain.output_chain)  # Should print [1.0, 2.0]
+    caller_chain.output_chain = []
