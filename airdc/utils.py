692d193a1aa9e9db5fbc296eca88c17bf249e514
OpenGHz <1352674740@qq.com>
2025-12-31 14:57:53 +0800

    fix: play without eef

--- airdc/utils.py
+++ airdc/utils.py
@@ -6,6 +6,9 @@ from pydantic import BaseModel, AliasChoices
 from mcap_data_loader.utils.basic import get_items_by_ext, zip, StrEnum
 
 
+zip_equal = zip
+
+
 class BaseModelWithFieldAliases(BaseModel):
     """By default, `validation_alias` will override the original field name.
     This class also adds the original field name to the alias."""
