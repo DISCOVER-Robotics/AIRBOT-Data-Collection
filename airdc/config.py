ebf56bb2aae8c3699de0a81feaeedce9a363ed55
Haizhou Ge <1352674740@qq.com>
2026-01-23 22:20:01 +0800

    simplify config

--- airdc/config.py
+++ airdc/config.py
@@ -12,32 +12,25 @@ from airdc.state_machine.fsm import (
 class DataCollectionConfig(BaseModel, frozen=True):
     """Configuration for the data collection."""
 
-    # the maximum rate for the managers
-    # 0 means as fast as possible
     update_rate: NonNegativeFloat = 0
-    # the finite state machine config
+    """the maximum rate for the managers
+    # 0 means as fast as possible"""
     fsm: DemonstrateFSMConfig
-    # managers to control the demonstrate actions
+    """the finite state machine config"""
     managers: ComponentsConfig
-    # log metrics
+    """managers to control the demonstrate actions"""
     log_metrics: int = -1
+    """log metrics every N seconds, -1 to disable"""
     log_jitter: bool = True
+    """whether to log jitter statistics"""
 
 
-class DataCollectionArgs(DemonstrateConfig):
+class DataCollectionArgs(DemonstrateConfig, DataCollectionConfig):
     """Top level arguments for the data collection.
     The structure is similar but not identical to
     `DataCollectionConfig` which is more suitable
     for the CLI configuration.
     """
 
-    # the maximum rate for the managers,
-    # 0 means as fast as possible
-    update_rate: NonNegativeFloat = 0
     # the finite state machine config
     fsm: StateMachineConfig
-    # managers to control the demonstrate actions
-    managers: ComponentsConfig
-    # log metrics
-    log_metrics: int = -1
-    log_jitter: bool = True
